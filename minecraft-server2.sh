4a59f695073e8650e1611f8cc24155ce7d96d137
Sijon Biswas <111903975+Sijon20@users.noreply.github.com>
2024-01-22 23:35:35 +0530

    Update minecraft-server2.sh

--- minecraft-server2.sh
+++ minecraft-server2.sh
@@ -9,7 +9,9 @@ JAVA_EXECUTABLE="java" # Make sure to set this to your Java path
 function check_screen {
     if ! command -v screen &> /dev/null
     then
-        echo "Screen could not be found. Please install it and run the script again."
+        #echo in red
+        tput setaf 1
+        echo "ERORR : Screen could not be found. Please install it and run the script again."
         exit
     fi
 }
@@ -18,7 +20,8 @@ function check_screen {
 function check_java {
     if ! command -v $JAVA_EXECUTABLE &> /dev/null
     then
-        echo "Java could not be found. Please install it and run the script again."
+        tput setaf 1
+        echo "ERORR : Java could not be found. Please install it and run the script again."
         exit
     fi
 }
@@ -32,7 +35,7 @@ function check_purpur {
         echo "Enter Purpur build to download (e.g. 100):"
         read purpur_build
         #if wget throws an error, exit the script
-        wget https://api.pl3x.net/v2/purpur/$purpur_version/$purpur_build/download -O $PURPUR_JAR || echo "Purpur jar file could not be found. Please make sure it's in the same directory as this script."
+        wget https://api.purpurmc.org/v2/purpur/$purpur_version/$purpur_build/download || echo "ERORR : Purpur jar file could not be found. Please make sure it's in the same directory as this script."
         exit
     fi
 }
